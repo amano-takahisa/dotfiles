bd68d557bea2a820dc606a9310f3d25fa96efeed
Taka <amano.takahisa@gmail.com>
2021-07-29 08:14:54 +0000

    add git root name to pane border

--- .config/tmux/pane-border-format.sh
+++ .config/tmux/pane-border-format.sh
@@ -2,6 +2,9 @@
 
 CHAR_BRANCH=$'\uE725'
 branch_name="$(git -C $1 rev-parse --abbrev-ref HEAD  2> /dev/null)"
+git_root_path="$(git -C $1 root  2> /dev/null)"
+[[ "$git_root_path" =~ .+/([^/]+)$ ]]
+git_root_name="${BASH_REMATCH[1]}"
 if [ -n "$branch_name" ] ; then
-    echo $CHAR_BRANCH $branch_name' '
+    echo $CHAR_BRANCH $branch_name@$git_root_name' '
 fi
