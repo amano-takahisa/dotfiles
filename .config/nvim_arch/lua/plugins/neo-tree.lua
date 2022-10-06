032f590f91a30bae3883803e7787a90b3383ec36
Taka <amano.takahisa@gmail.com>
2022-10-06 09:33:03 +0200

    indent file

--- .config/nvim_arch/lua/plugins/neo-tree.lua
+++ .config/nvim_arch/lua/plugins/neo-tree.lua
@@ -8,24 +8,24 @@ local plug_ok, plug = pcall(require, "neo-tree")
 if not plug_ok then return end
 
 plug.setup({
-        close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-        popup_border_style = "rounded",
-        enable_git_status = true,
-        enable_diagnostics = true,
-        sort_case_insensitive = false, -- used when sorting files and directories in the tree
-        sort_function = nil , -- use a custom function for sorting files and directories in the tree 
-        -- sort_function = function (a,b)
-        --       if a.type == b.type then
-        --           return a.path > b.path
-        --       else
-        --           return a.type > b.type
-        --       end
-        --   end , -- this sorts files and directories descendantly
-        default_component_configs = {
-          container = {
+    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+    popup_border_style = "rounded",
+    enable_git_status = true,
+    enable_diagnostics = true,
+    sort_case_insensitive = false, -- used when sorting files and directories in the tree
+    sort_function = nil , -- use a custom function for sorting files and directories in the tree 
+    -- sort_function = function (a,b)
+    --       if a.type == b.type then
+    --           return a.path > b.path
+    --       else
+    --           return a.type > b.type
+    --       end
+    --   end , -- this sorts files and directories descendantly
+    default_component_configs = {
+        container = {
             enable_character_fade = true
-          },
-          indent = {
+        },
+        indent = {
             indent_size = 2,
             padding = 1, -- extra padding on left hand side
             -- indent guides
@@ -38,8 +38,8 @@ plug.setup({
             expander_collapsed = "",
             expander_expanded = "",
             expander_highlight = "NeoTreeExpander",
-          },
-          icon = {
+        },
+        icon = {
             folder_closed = "",
             folder_open = "",
             folder_empty = "ﰊ",
@@ -47,40 +47,40 @@ plug.setup({
             -- then these will never be used.
             default = "*",
             highlight = "NeoTreeFileIcon"
-          },
-          modified = {
+        },
+        modified = {
             symbol = "[+]",
             highlight = "NeoTreeModified",
-          },
-          name = {
+        },
+        name = {
             trailing_slash = false,
             use_git_status_colors = true,
             highlight = "NeoTreeFileName",
-          },
-          git_status = {
-            symbols = {
-              -- Change type
-              added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
-              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-              deleted   = "✖",-- this can only be used in the git_status source
-              renamed   = "",-- this can only be used in the git_status source
-              -- Status type
-              untracked = "",
-              ignored   = "",
-              unstaged  = "",
-              staged    = "",
-              conflict  = "",
-            }
-          },
         },
-        window = {
-          position = "left",
-          width = 40,
-          mapping_options = {
+        git_status = {
+            symbols = {
+                -- Change type
+                added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                deleted   = "✖",-- this can only be used in the git_status source
+                renamed   = "",-- this can only be used in the git_status source
+                -- Status type
+                untracked = "",
+                ignored   = "",
+                unstaged  = "",
+                staged    = "",
+                conflict  = "",
+            }
+        },
+    },
+    window = {
+        position = "left",
+        width = 40,
+        mapping_options = {
             noremap = true,
             nowait = true,
-          },
-          mappings = {
+        },
+        mappings = {
             ["<space>"] = { 
                 "toggle_node", 
                 nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use 
@@ -102,11 +102,11 @@ plug.setup({
             ["z"] = "close_all_nodes",
             --["Z"] = "expand_all_nodes",
             ["a"] = { 
-              "add",
-              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-              config = {
-                show_path = "none" -- "none", "relative", "absolute"
-              }
+                "add",
+                -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                config = {
+                    show_path = "none" -- "none", "relative", "absolute"
+                }
             },
             ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add".
             ["d"] = "delete",
@@ -127,83 +127,83 @@ plug.setup({
             ["?"] = "show_help",
             ["<"] = "prev_source",
             [">"] = "next_source",
-          }
-        },
-        nesting_rules = {},
-        filesystem = {
-          filtered_items = {
+        }
+    },
+    nesting_rules = {},
+    filesystem = {
+        filtered_items = {
             visible = true, -- when true, they will just be displayed differently than normal items
             hide_dotfiles = false,
             hide_gitignored = false,
             hide_hidden = false, -- only works on Windows for hidden files/directories
             hide_by_name = {
-              --"node_modules"
+                --"node_modules"
             },
             hide_by_pattern = { -- uses glob style patterns
-              --"*.meta",
-              --"*/src/*/tsconfig.json",
+                --"*.meta",
+                --"*/src/*/tsconfig.json",
             },
             always_show = { -- remains visible even if other settings would normally hide it
-              --".gitignored",
+                --".gitignored",
             },
             never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-              --".DS_Store",
-              --"thumbs.db"
+                --".DS_Store",
+                --"thumbs.db"
             },
             never_show_by_pattern = { -- uses glob style patterns
-              --".null-ls_*",
+                --".null-ls_*",
             },
-          },
-          follow_current_file = false, -- This will find and focus the file in the active buffer every
-                                       -- time the current file is changed while the tree is open.
-          group_empty_dirs = false, -- when true, empty folders will be grouped together
-          hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-                                                  -- in whatever position is specified in window.position
-                                -- "open_current",  -- netrw disabled, opening a directory opens within the
-                                                  -- window like netrw would, regardless of window.position
-                                -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-          use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
-                                          -- instead of relying on nvim autocmd events.
-          window = {
-            mappings = {
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
-              ["H"] = "toggle_hidden",
-              ["/"] = "fuzzy_finder",
-              ["D"] = "fuzzy_finder_directory",
-              ["f"] = "filter_on_submit",
-              ["<c-x>"] = "clear_filter",
-              ["[g"] = "prev_git_modified",
-              ["]g"] = "next_git_modified",
-            }
-          }
         },
-        buffers = {
-          follow_current_file = true, -- This will find and focus the file in the active buffer every
-                                       -- time the current file is changed while the tree is open.
-          group_empty_dirs = true, -- when true, empty folders will be grouped together
-          show_unloaded = true,
-          window = {
+        follow_current_file = false, -- This will find and focus the file in the active buffer every
+        -- time the current file is changed while the tree is open.
+        group_empty_dirs = false, -- when true, empty folders will be grouped together
+        hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+        -- in whatever position is specified in window.position
+        -- "open_current",  -- netrw disabled, opening a directory opens within the
+        -- window like netrw would, regardless of window.position
+        -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+        use_libuv_file_watcher = false, -- This will use the OS level file watchers to detect changes
+        -- instead of relying on nvim autocmd events.
+        window = {
             mappings = {
-              ["bd"] = "buffer_delete",
-              ["<bs>"] = "navigate_up",
-              ["."] = "set_root",
+                ["<bs>"] = "navigate_up",
+                ["."] = "set_root",
+                ["H"] = "toggle_hidden",
+                ["/"] = "fuzzy_finder",
+                ["D"] = "fuzzy_finder_directory",
+                ["f"] = "filter_on_submit",
+                ["<c-x>"] = "clear_filter",
+                ["[g"] = "prev_git_modified",
+                ["]g"] = "next_git_modified",
             }
-          },
+        }
+    },
+    buffers = {
+        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        -- time the current file is changed while the tree is open.
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
+        show_unloaded = true,
+        window = {
+            mappings = {
+                ["bd"] = "buffer_delete",
+                ["<bs>"] = "navigate_up",
+                ["."] = "set_root",
+            }
         },
-        git_status = {
-          window = {
+    },
+    git_status = {
+        window = {
             position = "float",
             mappings = {
-              ["A"]  = "git_add_all",
-              ["gu"] = "git_unstage_file",
-              ["ga"] = "git_add_file",
-              ["gr"] = "git_revert_file",
-              ["gc"] = "git_commit",
-              ["gp"] = "git_push",
-              ["gg"] = "git_commit_and_push",
+                ["A"]  = "git_add_all",
+["gu"] = "git_unstage_file",
+                ["ga"] = "git_add_file",
+                ["gr"] = "git_revert_file",
+                ["gc"] = "git_commit",
+                ["gp"] = "git_push",
+                ["gg"] = "git_commit_and_push",
             }
-          }
         }
-      })
+    }
+})
 
