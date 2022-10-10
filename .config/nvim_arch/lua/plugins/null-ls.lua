eb427c873af2cdb7e3f64e855c0dbb230a42ca4e
Taka <amano.takahisa@gmail.com>
2022-10-10 15:00:45 +0200

    add autopep8

--- .config/nvim_arch/lua/plugins/null-ls.lua
+++ .config/nvim_arch/lua/plugins/null-ls.lua
@@ -2,11 +2,18 @@ local plug_ok, plug = pcall(require, "null-ls")
 if not plug_ok then return end
 
 
+
 plug.setup({
-        sources = {
+    root_dir = git_root_dir,
+    sources = {
         plug.builtins.formatting.stylua,
         plug.builtins.diagnostics.eslint,
         plug.builtins.completion.spell,
-        plug.builtins.formatting.isort,
+        -- plug.builtins.formatting.isort,
+        plug.builtins.formatting.autopep8.with({
+            'aggressive',
+            'aggressive'
+        }),
+
     },
 })
