cd1a7d8526458238269961dab8ec46b7a141fa11
Taka <amano.takahisa@gmail.com>
2022-10-12 10:04:04 +0200

    change lua lsp server

--- .config/nvim_arch/lua/plugins/null-ls.lua
+++ .config/nvim_arch/lua/plugins/null-ls.lua
@@ -6,7 +6,7 @@ if not plug_ok then return end
 plug.setup({
     root_dir = git_root_dir,
     sources = {
-        plug.builtins.formatting.stylua,
+        plug.builtins.formatting.sumneko_lua,
         plug.builtins.diagnostics.eslint,
         plug.builtins.completion.spell,
         -- plug.builtins.formatting.isort,
