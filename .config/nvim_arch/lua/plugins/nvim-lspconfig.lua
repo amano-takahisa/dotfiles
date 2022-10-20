3cf2d28219a36ccaed022318104ac9a9c4d7597a
Taka <amano.takahisa@gmail.com>
2022-10-20 23:12:26 +0200

    replace deplicated function

--- .config/nvim_arch/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim_arch/lua/plugins/nvim-lspconfig.lua
@@ -37,7 +37,7 @@ local lsp_flags = {
     debounce_text_changes = 150,
 }
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 -- require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
 require('lspconfig')['pyright'].setup {
