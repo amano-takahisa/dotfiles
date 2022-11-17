4145214b6d6c6fddad51b3b0a676f950e6773238
Taka <amano.takahisa@gmail.com>
2022-11-17 11:46:56 +0100

    auto completion from all buffers instead of visual buffers

--- .config/nvim_arch/lua/plugins/nvim-cmp.lua
+++ .config/nvim_arch/lua/plugins/nvim-cmp.lua
@@ -40,11 +40,7 @@ cmp.setup({
             name = 'buffer',
             option = {
                 get_bufnrs = function()
-                    local bufs = {}
-                    for _, win in ipairs(vim.api.nvim_list_wins()) do
-                        bufs[vim.api.nvim_win_get_buf(win)] = true
-                    end
-                    return vim.tbl_keys(bufs)
+                    return vim.api.nvim_list_bufs()
                 end
             }
         },
