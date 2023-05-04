f465756c3a6b6b79e66f1100d26c8ec4a75c9925
Taka <amano.takahisa@gmail.com>
2023-05-04 12:28:49 +0200

    remove live_grep

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ local lga_actions = require("telescope-live-grep-args.actions")
 -- shortcut key
 local keymap = vim.keymap.set
 keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+-- keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
 
