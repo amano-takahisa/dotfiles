84646f9ed3896caa6d81c61fceddc4948f848bfe
Taka <amano.takahisa@gmail.com>
2023-10-10 16:39:43 +0200

    update setup

--- setup_arch.sh
+++ setup_arch.sh
@@ -16,32 +16,31 @@
 #
 # Then, install and setup git, firefox, etc.
 #
+# ```bash
+# sudo pacman -Syyu --noconfirm --needed neovim python-neovim xclip wl-clipboard firefox openssh git github-cli
+# gh auth login -p ssh -h github.com -w
 # ```
-# sudo pacman -Syyu neovim python-neovim xclip wl-clipboard firefox openssh git
-# ssh-keygen -t ed25519 -C "amano.takahisa@gmail.com" -f ~/.ssh/id_ed25519 -q -N "" \
-#     && eval "$(ssh-agent -s)" \
-#     && ssh-add ~/.ssh/id_ed25519 \
-#     && cat ~/.ssh/id_ed25519.pub | wl-copy
-# ```
-# and login GitHub and paste copied ssh key to
-# https://github.com/settings/ssh/new
 # Then,
 # ```
 # mkdir -p ~/Documents/git \
-#     && mkdir -p ~/.config \
-#     && git clone "git@github.com:amano-takahisa/dotfiles.git" ~/Documents/git/dotfiles
+#     && git clone "git@github.com:amano-takahisa/dotfiles.git" ~/Documents/git/dotfiles \
 #     && ln -sf ~/Documents/git/dotfiles/.config/git ~/.config/git
 # ```
+# and run followings with sudo
 
 set -euxo pipefail
 
 USER='takahisa'
 USER_HOME=/home/"${USER}"
-DOTFILES_REPO="${USER_HOME}"/Documents/git/dotfiles
+REPOS_DIR="${USER_HOME}"/Documents/git
+DOTFILES_REPO="${REPOS_DIR}"/dotfiles
 
-PATH="${USER_HOME}/.local/bin:${PATH}"
+PATH="${USER_HOME}/.local/bin:${USER_HOME}/bin:${PATH}"
 
 cd "${USER_HOME}"
+
+####### gpg key #######
+sudo -u "${USER}" curl -sS https://github.com/web-flow.gpg | sudo -u "${USER}" gpg --import -
 
 ####### dotfiles #######
 sudo -u "${USER}" mkdir -p "${USER_HOME}"/.config/
@@ -50,69 +49,26 @@ sudo -u "${USER}" mkdir -p "${USER_HOME}"/.config/
 sudo -u "${USER}" mkdir -p "${USER_HOME}"/bin/
 
 ####### Clone repositories #######
-declare -a repos=(
-    "git@github.com:amano-takahisa/mypo.git"
-    "git@github.com:amano-takahisa/gravel.git"
-    "git@github.com:amano-takahisa/poipoi.git"
-    "git@github.com:amano-takahisa/numheader.git"
-    "git@github.com:amano-takahisa/czkawka.git"
-)
-
+sudo -u "${USER}" git clone "git@github.com:amano-takahisa/multigit.git" "${USER_HOME}"/Documents/git/multigit
+sudo -u "${USER}" ln -sf "${REPOS_DIR}"/multigit/multigit.py "${USER_HOME}"/bin/mg
 cd ${USER_HOME}/Documents/git
-for repo in "${repos[@]}"; do
-    sudo -u "${USER}" git clone "${repo}"
-    IFS='/.' read -ra parts <<< "${repo}"
-    cd "${parts[-2]}"
-    sudo -u "${USER}" pre-commit install
-    cd ../
-done
+sudo -u "${USER}" mg clone -u amano-takahisa
 cd "${USER_HOME}"
 
-####### po #######
-sudo -u "${USER}" ln -sf "${USER_HOME}"/Documents/git/mypo/utils/po.py \
-    "${USER_HOME}"/bin/po
-
-####### numheader #######
-sudo -u "${USER}" ln -sf "${USER_HOME}"/Documents/git/numheader/numheader/numheader.py \
-    "${USER_HOME}"/bin/numheader
-
-####### zsh #######
-pacman -S --noconfirm --needed \
-    zsh zsh-completions
-
-# ohmyzsh
-sudo -u "${USER}" ZSH="${USER_HOME}/Documents/git/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.zshenv "${USER_HOME}"
-sudo -u "${USER}" mkdir "${USER_HOME}"/.config/
-sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.config/zsh "${USER_HOME}"/.config/
-rm "${USER_HOME}"/.zshrc
-
-# zsh theme
-sudo -u "${USER}" git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${USER_HOME}/Documents/git/oh-my-zsh/custom/themes/powerlevel10k
-
-# zsh custom
-sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/misc/omz_custom/* \
-    "${USER_HOME}"/Documents/git/oh-my-zsh/custom/
 
 
-####### package manager #######
+##### package manager #######
 pacman -S --noconfirm --needed \
     base-devel
-sudo -u "${USER}" git clone https://aur.archlinux.org/yay.git ${USER_HOME}/Documents/git/yay \
-    && cd yay \
-    && makepkg -si
+sudo -u "${USER}" git clone https://aur.archlinux.org/yay-bin.git ${USER_HOME}/Documents/git/yay
+cd ${REPOS_DIR}/yay
+sudo -u "${USER}"  makepkg -si
+# <----   HERE root password will be asked. ----> #
 cd "${USER_HOME}"
 
 ####### git #######
-sudo -u "${USER}" yay -Yg \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     git-secrets
-
-# pacman -S --noconfirm --needed \
-#     lazygit
-
-pacman -S --noconfirm --needed \
-    github-cli
 
 ####### neovim #######
 sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.config/nvim "${USER_HOME}"/.config/nvim
@@ -120,7 +76,7 @@ sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.config/nvim "${USER_HOME}"/.config/
 ####### tmux #######
 pacman -S --noconfirm --needed \
     tmux
-sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.tmux.conf "${USER_HOME}"/.tmux.conf
+# sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.tmux.conf "${USER_HOME}"/.tmux.conf
 
 ####### XDG #######
 pacman -S --noconfirm --needed \
@@ -174,9 +130,10 @@ echo 'ja_JP.UTF-8 UTF-8' | tee --append /etc/locale.gen
 pacman -S --noconfirm --needed \
     otf-ipafont otf-ipaexfont  ttf-hack-nerd \
     ttf-mplus-nerd ttf-noto-nerd
+
 # IME
-sudo -u "${USER}" yay -Yg \
-    fcitx5-mozc-ut
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
+    --repo mozc fcitx5-mozc-ut
 pacman -S --noconfirm --needed \
     fcitx5-configtool
 
@@ -184,36 +141,37 @@ pacman -S --noconfirm --needed \
 pacman -S --noconfirm --needed \
     python-pip
 
-pacman -S --noconfirm --needed \
-    pyright
+# pacman -S --noconfirm --needed \
+#     pyright
+# 
+# pacman -S --noconfirm --needed \
+#     pyenv
 
-pacman -S --noconfirm --needed \
-    pyenv
-
-sudo -u "${USER}" pip install pylama autopep8
+# sudo -u "${USER}" pip install pylama autopep8
 
 # conda environment
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     miniconda3
 ln -sf /opt/miniconda3/etc/profile.d/conda.sh /etc/profile.d/conda.sh
 
 sudo -u "${USER}" /opt/miniconda3/bin/conda init zsh
 
 # Thonny for raspberry pi pico
-sudo -u "${USER}" paru -S --noconfirm --needed \
-    thonny esptool python-ptyprocess python-build
-# add to uucp group to communicate pico
-usermod -a -G uucp "${USER}"
+# sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
+#     thonny esptool python-ptyprocess python-build
+# # add to uucp group to communicate pico
+# usermod -a -G uucp "${USER}"
 
 ####### R #######
 pacman -S --noconfirm --needed \
     r
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     rstudio-desktop-bin r-tinytex
 
-####### Rust #######
-sudo -u "${USER}" curl --proto '=https' --tlsv1.2 -sSf \
-    https://sh.rustup.rs | sh
+# ####### Rust #######
+# # Need to find a way to run this non-interactive
+# sudo -u "${USER}" curl --proto '=https' --tlsv1.2 -sSf \
+#     https://sh.rustup.rs | sh
 
 ####### GIS/RS #######
 # QGIS
@@ -221,15 +179,15 @@ pacman -S --noconfirm --needed \
     qgis python-yaml python-gdal python-psycopg2
 # sudo -u "${USER}" pip install Jinja2 Pygments owslib
 # GRASS GIS
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     grass
 
 ####### Web browser #######
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     google-chrome
 
 ####### Slack #######
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     slack-desktop
 
 ####### Office #######
@@ -242,20 +200,20 @@ pacman -S --noconfirm --needed \
     spectacle
 
 ####### Node #######
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     nvm
 pacman -S --noconfirm --needed \
         npm
 
 ####### node tools #######
-sudo -u "${USER}" npm install markdownlint --save-dev
-npm install -g markdownlint-cli
+# sudo -u "${USER}" npm install markdownlint --save-dev
+# npm install -g markdownlint-cli
 ####### tools #######
 pacman -S --noconfirm --needed \
     jq wget rsync
 
 # disk management
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     qdirstat
 
 ####### aws cli #######
@@ -276,44 +234,45 @@ pacman -S --noconfirm --needed \
 pacman -S --noconfirm --needed \
     ksysguard
 
-####### pre-commit #######
-sudo -u "${USER}" pip install pre-commit
-
-####### backup #######
-pacman -S --noconfirm --needed \
-    snapper
-
+# ####### pre-commit #######
+# sudo -u "${USER}" pip install pre-commit
+# 
+# ####### backup #######
+# pacman -S --noconfirm --needed \
+#     snapper
+# 
 ####### memo #######
 pacman -S --noconfirm --needed \
     zk
+sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.config/zk "${USER_HOME}"/.config/zk
 
 ####### Document viewer #######
 pacman -S --noconfirm --needed \
     okular
 
-####### Document management #######
-sudo -u "${USER}" paru -S --noconfirm --needed \
-    zotero
-
-####### Image viewer #######
-sudo -u "${USER}" paru -S --noconfirm --needed \
-    gwenview
-
+# ####### Document management #######
+# sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
+#     zotero
+# 
+# ####### Image viewer #######
+# sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
+#     gwenview
+# 
 ####### media #######
 pacman -S --noconfirm --needed \
     vlc
 
-####### cloud services #######
-# rclone
-pacman -S --noconfirm --needed \
-    rclone fuse2
-# make dir for mount
-sudo -u "${USER}" mkdir "${USER_HOME}"/google_drive
-# to mount the drive
-# rclone mount gd:/ /home/takahisa/google_drive --daemon --vfs-cache-mode full
-# to unmount, run
-# fusermount -u /home/takahisa/google_drive
-
+# ####### cloud services #######
+# # rclone
+# pacman -S --noconfirm --needed \
+#     rclone fuse2
+# # make dir for mount
+# sudo -u "${USER}" mkdir "${USER_HOME}"/google_drive
+# # to mount the drive
+# # rclone mount gd:/ /home/takahisa/google_drive --daemon --vfs-cache-mode full
+# # to unmount, run
+# # fusermount -u /home/takahisa/google_drive
+# 
 ####### Devices #######
 # Logicool mouse
 pacman -S --noconfirm --needed \
@@ -325,21 +284,22 @@ pacman -S --noconfirm --needed \
     kdegraphics-thumbnailers
 
 # ####### speech #######
-# sudo -u "${USER}" paru -S --noconfirm --needed \
+# sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
 #      speech-dispatcher festival espeak-ng
 #
 # sudo -u "${USER}" pip install TTS
 
-####### Disk management #######
-# czkawka
-# use my fork instead of official one
-# pacman -S --noconfirm --needed \
+# ####### Disk management #######
+# # czkawka
+# # need to find a way to install non-interactive
+# sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
 #     czkawka-gui
-pacman -S --noconfirm --needed \
-    gtk4
-cd ${USER_HOME}/Documents/git/czkawka
-sudo -u "${USER}" cargo install czkawka_gui
-cd "${USER_HOME}"
+# # use my fork instead of official one
+# # pacman -S --noconfirm --needed \
+# #     gtk4
+# # cd ${USER_HOME}/Documents/git/czkawka
+# # sudo -u "${USER}" cargo install czkawka_gui
+# # cd "${USER_HOME}"
 
 pacman -S --noconfirm --needed \
     partitionmanager
@@ -348,7 +308,7 @@ pacman -S --noconfirm --needed \
 pacman -S --noconfirm --needed \
     lsof
 
-sudo -u "${USER}" paru -S --noconfirm --needed \
+sudo -u "${USER}" echo y | sudo -u "${USER}" yay -S --sudoloop --answerclean None --answerdiff None \
     realvnc-vnc-viewer
 
 ####### OBS #######
@@ -357,18 +317,36 @@ pacman -S --noconfirm --needed \
 # and follow step 2 of
 # https://wiki.archlinux.org/title/V4l2loopback
 
-####### system monitor #######
-pacman -S --noconfirm --needed \
-    netdata
-systemctl start netdata
-systemctl enable netdata
-
+# ####### system monitor #######
+# pacman -S --noconfirm --needed \
+#     netdata
+# systemctl start netdata
+# systemctl enable netdata
+# 
 ####### wine #######
 echo '[multilib]' >> /etc/pacman.conf
-echo 'Include = /etc/pacman.d/mirrorlist'
-pacman -S --noconfirm --needed \
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+pacman -Syyu --noconfirm --needed \
     wine
 
 ####### imagemagick #######
 pacman -S --noconfirm --needed \
     imagemagick
+
+####### zsh #######
+pacman -S --noconfirm --needed \
+    zsh zsh-completions
+
+# ohmyzsh
+sudo -u "${USER}" ZSH="${USER_HOME}/Documents/git/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.zshenv "${USER_HOME}"
+sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/.config/zsh "${USER_HOME}"/.config/
+rm -f "${USER_HOME}"/.zshrc
+
+# zsh theme
+sudo -u "${USER}" git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${USER_HOME}/Documents/git/oh-my-zsh/custom/themes/powerlevel10k
+
+# zsh custom
+sudo -u "${USER}" ln -sf "${DOTFILES_REPO}"/misc/omz_custom/* \
+    "${USER_HOME}"/Documents/git/oh-my-zsh/custom/
