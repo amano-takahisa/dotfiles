a3e97cc0db99f88581354a579e7e9162e40bbae6
Taka <amano.takahisa@gmail.com>
2023-10-15 10:33:48 +0200

    add doge

---
+++ .config/nvim/lua/plugins/vim-doge.lua
@@ -0,0 +1,13 @@
+return {
+    "kkoomen/vim-doge",
+    event = "BufRead",
+    config = function()
+        vim.cmd([[call doge#install()]])
+        vim.g.doge_doc_standard_python = "numpy"
+
+        vim.g.doge_python_settings = {
+            single_quotes = 0,
+            omit_redundant_param_types = 1,
+        }
+    end,
+}
