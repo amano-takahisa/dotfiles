4d9063a5312b1990a27e1fbf7630c720b6e4d9d3
Taka <amano.takahisa@gmail.com>
2024-02-25 15:24:31 +0100

    change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,7 +15,7 @@ config.font = wezterm.font 'HackGen Console NF'
 -- config.font = wezterm.font 'JetBrains Mono NL'
 -- 12324567890,.+-*/=;:<>[]{}()!@#$%^&*~`"'?_|
 -- abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-config.font_size = 8.0
+config.font_size = 9.0
 config.freetype_load_target = "HorizontalLcd"
 config.color_scheme = 'MaterialOcean'
 config.window_frame = {
