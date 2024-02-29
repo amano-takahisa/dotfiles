141a606cce140368c8f674e225aa4032452fa547
Taka <amano.takahisa@gmail.com>
2024-02-29 14:57:31 +0100

    update font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,44 +7,49 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'HackGen Console NF'
+config.font = wezterm.font("HackGen Console NF")
 -- config.font = wezterm.font 'JetBrains Mono NL'
 -- 12324567890,.+-*/=;:<>[]{}()!@#$%^&*~`"'?_|
 -- abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 config.font_size = 9.0
 config.freetype_load_target = "HorizontalLcd"
-config.color_scheme = 'MaterialOcean'
+config.color_scheme = "MaterialOcean"
 config.window_frame = {
-  -- The font used in the tab bar.
-  -- Roboto Bold is the default; this font is bundled
-  -- with wezterm.
-  -- Whatever font is selected here, it will have the
-  -- main font setting appended to it to pick up any
-  -- fallback fonts you may have used there.
-  font = wezterm.font { family = 'HackGen35 Console NF', weight = 'Regular' },
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font_with_fallback({
+		{ family = "HackGen35 Console NF", weight = "Regular" },
+		{ family = "Noto Sans CJK JP", weight = "Regular" },
+        { family = "Noto Color Emoji" },
+        { family = "Noto Emoji" },
+	}),
 
-  -- The size of the font in the tab bar.
-  -- Default to 10.0 on Windows but 12.0 on other systems
-  font_size = 7.0,
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 7.0,
 
-  -- The overall background color of the tab bar when
-  -- the window is focused
-  active_titlebar_bg = '#333333',
+	-- The overall background color of the tab bar when
+	-- the window is focused
+	active_titlebar_bg = "#333333",
 
-  -- The overall background color of the tab bar when
-  -- the window is not focused
-  inactive_titlebar_bg = '#333333',
+	-- The overall background color of the tab bar when
+	-- the window is not focused
+	inactive_titlebar_bg = "#333333",
 }
 
 config.colors = {
-  tab_bar = {
-    -- The color of the inactive tab bar edge/divider
-    inactive_tab_edge = '#575757',
-  },
+	tab_bar = {
+		-- The color of the inactive tab bar edge/divider
+		inactive_tab_edge = "#575757",
+	},
 }
 -- and finally,. return the configuration to wezterm
 return config
