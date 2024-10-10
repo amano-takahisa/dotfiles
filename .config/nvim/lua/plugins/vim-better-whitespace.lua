9a1ece2382019828f40a4790ddc74e841d4bd8d1
Taka <amano.takahisa@gmail.com>
2024-10-10 13:33:29 +0200

    add whitespace plugins

---
+++ .config/nvim/lua/plugins/vim-better-whitespace.lua
@@ -0,0 +1,6 @@
+vim.g.better_whitespace_enabled = 1
+vim.g.better_whitespace_filetypes_blacklist = { "diff", "gitcommit" }
+
+return {
+	"ntpeters/vim-better-whitespace",
+}
---
+++ .config/nvim/lua/plugins/visual-whitespace.lua
@@ -0,0 +1,11 @@
+return {
+    "mcauley-penney/visual-whitespace.nvim",
+    config = true,
+    opts = {
+        highlight = { link = "Visual" },
+        space_char = "·",
+        tab_char = "→",
+        nl_char = "↲",
+        cr_char = "←",
+    },
+}
