62093edb1afbddb7ac11bd80a8e1a9f0fbae8b5d
Taka <amano.takahisa@gmail.com>
2024-11-15 18:39:41 +0100

    use flash instead of hop

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,62 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      char = {
+        jump_labels = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump({
+          label = { before = true, after = false },
+        })
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+    {
+      "ww",
+      mode = { "n", "x", "o" },
+      function() end,
+      desc = "jump any word",
+    },
+  },
+}
