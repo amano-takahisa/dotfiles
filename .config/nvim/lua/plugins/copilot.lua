2e54363e74cff1495b489cb772433552220e61a1
Taka <amano.takahisa@gmail.com>
2024-11-15 18:39:28 +0100

    move copilot config into lazy

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,11 +1,12 @@
-vim.g.copilot_filetypes = {
-    gitcommit = true,
-    markdown = true,
-    yaml = true,
-}
-
 return {
     {
         "github/copilot.vim",
+        config = function()
+            vim.g.copilot_filetypes = {
+                gitcommit = true,
+                markdown = true,
+                yaml = true,
+            }
+        end,
     },
 }
