429e9366fd1e4b56de5021deaab16937b0f75225
Taka <amano.takahisa@gmail.com>
2024-11-29 12:40:27 +0100

    nvim tab handling

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,57 +1,57 @@
-vim.g.python3_host_prog = '/home/takahisa/Documents/venvs/neovim/.venv/bin/python'
+vim.g.python3_host_prog = "/home/takahisa/Documents/venvs/neovim/.venv/bin/python"
 
 local options = {
-    -- UI left
-    number = true,
-    numberwidth = 2,
-    relativenumber = true,
+        -- UI left
+        number = true,
+        numberwidth = 2,
+        relativenumber = true,
 
-    -- UI top
-    title = true,
+        -- UI top
+        title = true,
 
-    -- UI right
+        -- UI right
 
-    -- UI bottom
-    cmdheight = 2, -- from v0.8, 0 is available.
+        -- UI bottom
+        cmdheight = 2, -- from v0.8, 0 is available.
 
+        -- Window
+        splitright = true,
+        splitbelow = true,
 
-    -- Window
-    splitright = true,
-    splitbelow = true,
+        -- mouse, clip board integration
+        clipboard = "unnamedplus",
+        mouse = "a",
 
-    -- mouse, clip board integration
-    clipboard = "unnamedplus",
-    mouse = "a",
+        -- cursor
+        cursorline = false,
+        scrolloff = 8,
+        sidescrolloff = 8,
 
-    -- cursor
-    cursorline = false,
-    scrolloff = 8,
-    sidescrolloff = 8,
+        -- floating window
+        winblend = 20,
+        pumblend = 20,
 
-    -- floating window
-    winblend = 20,
-    pumblend = 20,
+        -- search
+        ignorecase = true,
+        smartcase = true,
 
-    -- search
-    ignorecase = true,
-    smartcase = true,
+        -- indent, tabs
+        smartindent = true,
+        -- smarttab = true,
+        expandtab = true,
+        -- shiftwidth = 4,
+        -- tabstop = 4,
 
-    -- indent, tabs
-    -- smartindent = true,
-    expandtab = true,
-    shiftwidth = 4,
-    tabstop = 4,
+        -- backup, swap
+        swapfile = true,
+        undofile = true,
+        -- do not change inode
+        backupcopy = "yes",
 
-    -- backup, swap
-    swapfile = true,
-    undofile = true,
-    -- do not change inode
-    backupcopy = "yes",
-
-    -- colors
-    -- termguicolors = true,
-    colorcolumn = "79,120"
+        -- colors
+        -- termguicolors = true,
+        colorcolumn = "79,120",
 }
 for k, v in pairs(options) do
-    vim.opt[k] = v
+        vim.opt[k] = v
 end
